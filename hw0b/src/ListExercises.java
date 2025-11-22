e92f3d2d5537ea4472de28aff7c46363540cd845
Moonhalf383 <YorozuTsuki@163.com>
2025-11-22 21:08:33 +0800

    hw0b completed.

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,29 +1,62 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
 
-    /** Returns the total sum in a list of integers */
-    public static int sum(List<Integer> L) {
-        // TODO: Fill in this function.
-        return 0;
+  /** Returns the total sum in a list of integers */
+  public static int sum(List<Integer> L) {
+    // TODO: Fill in this function.
+    int sum = 0;
+    for (int i : L) {
+      sum += i;
     }
+    return sum;
+  }
 
-    /** Returns a list containing the even numbers of the given list */
-    public static List<Integer> evens(List<Integer> L) {
-        // TODO: Fill in this function.
-        return null;
+  /** Returns a list containing the even numbers of the given list */
+  public static List<Integer> evens(List<Integer> L) {
+    // TODO: Fill in this function.
+    List<Integer> evenList = new ArrayList<>();
+    for (Integer i : L) {
+      if (i % 2 == 0)
+        evenList.add(i);
     }
+    return evenList;
+  }
 
-    /** Returns a list containing the common item of the two given lists */
-    public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        // TODO: Fill in this function.
-        return null;
+  /** Returns a list containing the common item of the two given lists */
+  public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
+    // TODO: Fill in this function.
+    List<Integer> commonList = new ArrayList<>();
+    for (Integer i1 : L1) {
+      for (Integer i2 : L2) {
+        if (i1 == i2) {
+          Boolean state = true;
+          for (Integer i3 : commonList) {
+            if (i3 == i1)
+              state = false;
+          }
+          if (state)
+            commonList.add(i1);
+        }
+      }
     }
+    return commonList;
+  }
 
-
-    /** Returns the number of occurrences of the given character in a list of strings. */
-    public static int countOccurrencesOfC(List<String> words, char c) {
-        // TODO: Fill in this function.
-        return 0;
+  /**
+   * Returns the number of occurrences of the given character in a list of
+   * strings.
+   */
+  public static int countOccurrencesOfC(List<String> words, char c) {
+    // TODO: Fill in this function.
+    int occurrences = 0;
+    for (String word : words) {
+      for (int i = 0; i < word.length(); i++) {
+        if (word.charAt(i) == c)
+          occurrences += 1;
+      }
     }
+    return occurrences;
+  }
 }
--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,26 +1,50 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class MapExercises {
-    /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
-     * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
-     */
-    public static Map<Character, Integer> letterToNum() {
-        // TODO: Fill in this function.
-        return null;
+  /**
+   * Returns a map from every lower case letter to the number corresponding to
+   * that letter, where 'a' is
+   * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
+   */
+  public static Map<Character, Integer> letterToNum() {
+    // TODO: Fill in this function.
+    Map<Character, Integer> ltnMap = new HashMap<>();
+    Character ch = 'a';
+    for (int i = 0; i < 26; i++) {
+      ltnMap.put((char) (ch + i), i + 1);
     }
+    return ltnMap;
+  }
 
-    /** Returns a map from the integers in the list to their squares. For example, if the input list
-     *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
-     */
-    public static Map<Integer, Integer> squares(List<Integer> nums) {
-        // TODO: Fill in this function.
-        return null;
+  /**
+   * Returns a map from the integers in the list to their squares. For example, if
+   * the input list
+   * is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to
+   * 49.
+   */
+  public static Map<Integer, Integer> squares(List<Integer> nums) {
+    // TODO: Fill in this function.
+    Map<Integer, Integer> sqMap = new HashMap<>();
+    for (Integer i : nums) {
+      sqMap.put(i, i * i);
     }
+    return sqMap;
+  }
 
-    /** Returns a map of the counts of all words that appear in a list of words. */
-    public static Map<String, Integer> countWords(List<String> words) {
-        // TODO: Fill in this function.
-        return null;
+  /** Returns a map of the counts of all words that appear in a list of words. */
+  public static Map<String, Integer> countWords(List<String> words) {
+    // TODO: Fill in this function.
+    Map<String, Integer> capMap = new HashMap<>();
+    for (String word : words) {
+      int occurrance = 0;
+      for (String tword : words) {
+        if (tword == word)
+          occurrance += 1;
+      }
+      capMap.put(word, occurrance);
     }
+    return capMap;
+  }
 }
