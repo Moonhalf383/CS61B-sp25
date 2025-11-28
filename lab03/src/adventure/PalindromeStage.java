f051f7289507a8873695e172eb92794ef6c1fb7c
Moonhalf383 <YorozuTsuki@163.com>
2025-11-28 21:05:26 +0800

    Lab03 completed, except for optional part which is not friendly for neovim.

--- lab03/src/adventure/PalindromeStage.java
+++ lab03/src/adventure/PalindromeStage.java
@@ -8,79 +8,81 @@ import java.util.TreeMap;
 
 public class PalindromeStage implements AdventureStage {
 
-    private final In in;
-    private final Map<String, AdventureStage> responses;
+  private final In in;
+  private final Map<String, AdventureStage> responses;
 
-    public PalindromeStage(In in) {
-        this.in = in;
-        AdventureStage nextStage = new FillerStage(
-                "Mm, tasty. Briefly, you wonder if free will is an illusion. You hear some people talking " +
-                        "about a machine in Soda and decide to check it out.",
-                Map.of("go", new MachineStage(in))
-        );
-        this.responses = new TreeMap<>(Map.of(
-                "va cafe", nextStage,
-                "hearst food court", nextStage
-        ));
+  public PalindromeStage(In in) {
+    this.in = in;
+    AdventureStage nextStage = new FillerStage(
+        "Mm, tasty. Briefly, you wonder if free will is an illusion. You hear some people talking " +
+            "about a machine in Soda and decide to check it out.",
+        Map.of("go", new MachineStage(in)));
+    this.responses = new TreeMap<>(Map.of(
+        "va cafe", nextStage,
+        "hearst food court", nextStage));
+  }
+
+  @Override
+  public void playStage() {
+    System.out.println("""
+        The Woz is a cool name, much better than "Soda 430". Soda 606 is a neat, symmetric conference room,
+        like its number. We could rename The Woz to be palindromic, but it sounds so cool! Why not change the
+        room number instead?
+        (Give a palindromic room number.)
+        """);
+    while (true) {
+      String input = in.readLine();
+      while (!AdventureUtils.isInt(input)) {
+        System.out.println("Please enter a valid integer.");
+        input = this.in.readLine();
+      }
+
+      IntList numLst = digitsToIntList(input);
+      IntList reversedLst = reverseList(numLst);
+
+      if (numLst.equals(reversedLst)) {
+        System.out.println("Wow, nice room number!");
+        break;
+      }
+
+      System.out.println("That's not a palindrome! Try again.");
     }
+  }
 
-    @Override
-    public void playStage() {
-        System.out.println("""
-                The Woz is a cool name, much better than "Soda 430". Soda 606 is a neat, symmetric conference room,
-                like its number. We could rename The Woz to be palindromic, but it sounds so cool! Why not change the
-                room number instead?
-                (Give a palindromic room number.)
-                """);
-        while (true) {
-            String input = in.readLine();
-            while (!AdventureUtils.isInt(input)) {
-                System.out.println("Please enter a valid integer.");
-                input = this.in.readLine();
-            }
+  @Override
+  public String nextStagePrompt() {
+    return "Hmm, you're getting hungry. Where do you want to go?";
+  }
 
-            IntList numLst = digitsToIntList(input);
-            IntList reversedLst = reverseList(numLst);
+  @Override
+  public Map<String, AdventureStage> getResponses() {
+    return responses;
+  }
 
-            if (numLst.equals(reversedLst)) {
-                System.out.println("Wow, nice room number!");
-                break;
-            }
-
-            System.out.println("That's not a palindrome! Try again.");
-        }
+  /**
+   * Returns a new IntList with the contents of the original IntList in reverse
+   * order.
+   */
+  private static IntList reverseList(IntList l) {
+    IntList reversed = null;
+    while (l != null) {
+      reversed = new IntList(l.first, reversed);
+      l = l.rest;
     }
+    return reversed;
+  }
 
-    @Override
-    public String nextStagePrompt() {
-        return "Hmm, you're getting hungry. Where do you want to go?";
+  /**
+   * Given an input string of digits, converts it into an IntList of single-digit
+   * ints.
+   * For example, the string "606" is converted to 6 -> 0 -> 6.
+   */
+  private static IntList digitsToIntList(String s) {
+    int[] a = new int[s.length()];
+    for (int i = s.length(); i > 0; i--) {
+      a[s.length() - i] = Character.getNumericValue(s.charAt(i - 1));
     }
-
-    @Override
-    public Map<String, AdventureStage> getResponses() {
-        return responses;
-    }
-
-    /** Returns a new IntList with the contents of the original IntList in reverse order.*/
-    private static IntList reverseList(IntList l) {
-        IntList reversed = null;
-        while (l.rest != null) {
-            reversed = new IntList(l.first, reversed);
-            l = l.rest;
-        }
-        return reversed;
-    }
-
-    /**
-     * Given an input string of digits, converts it into an IntList of single-digit ints.
-     * For example, the string "606" is converted to 6 -> 0 -> 6.
-     */
-    private static IntList digitsToIntList(String s) {
-        int[] a = new int[s.length()];
-        for (int i = s.length(); i > 0; i++) {
-            a[s.length() - i] = Character.getNumericValue(s.charAt(i));
-        }
-        return IntList.of(a);
-    }
+    return IntList.of(a);
+  }
 
 }
